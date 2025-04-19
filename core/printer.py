774e950bd9d7ea44d9067dbb19bf5079903577b2
grigory-zhovtun <grigorii.zhovtun@gmail.com>
2025-04-19 22:46:25 +0500

    use items()

--- core/printer.py
+++ core/printer.py
@@ -10,14 +10,12 @@ def print_statistics_table(stats_by_language, title):
             "Средняя зарплата",
         ]
     ]
-    for language in stats_by_language:
-        headers.append(
-            [
-                language,
-                stats_by_language[language]["vacancies_found"],
-                stats_by_language[language]["vacancies_processed"],
-                stats_by_language[language]["average_salary"],
-            ]
-        )
+    for language, stats in stats_by_language.items():
+        headers.append([
+            language,
+            stats["vacancies_found"],
+            stats["vacancies_processed"],
+            stats["average_salary"],
+        ])
     table_instance = AsciiTable(headers, title)
     print(table_instance.table)
