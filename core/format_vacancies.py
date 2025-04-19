d49ebd3d052d7c3c8afaf56384513f78c69b297b
grigory-zhovtun <grigorii.zhovtun@gmail.com>
2025-04-19 23:03:29 +0500

    union format functions to single function for all objects (hh and sj)

---
+++ core/format_vacancies.py
@@ -0,0 +1,21 @@
+def format_vacancies(vacancies_by_language, predict_salary_fn):
+    formatted = {}
+    for language, vacancies_raw in vacancies_by_language.items():
+        found = vacancies_raw.get("found", vacancies_raw.get("total", 0))
+        vacancies = vacancies_raw.get("items", [])
+
+        vacs = [
+            {
+                "title": vacancy.get("name")       # HH: name
+                         or vacancy.get("profession"),  # SJ: profession
+                "salary": predict_salary_fn(vacancy),
+            }
+            for vacancy in vacancies
+        ]
+
+        formatted[language] = {
+            "found": found,
+            "vacancies": vacs
+        }
+
+    return formatted
